9c57b7e832a07d4eb3e7733dda2d43cbd4391d88
Gavin Flood <gavin@gavinflood.com>
2020-07-20 21:07:20 +0100

    Changed messaging on some pages

--- client/src/components/about/about-view.js
+++ client/src/components/about/about-view.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about-view.css";
 
-const AboutView = props => {
+const AboutView = (props) => {
   return (
     <div>
       <h1>About</h1>
@@ -11,19 +11,19 @@ const AboutView = props => {
         Ireland.
       </p>
       <p>
-        I have over 7 years of experience working in a range of different roles,
-        mostly customer-facing. I currently work with Guidewire, working as a
-        senior developer with some of our customers deployed in the cloud.
+        I have over 7 years of experience working in a range of different roles
+        in the software industry. I currently work with Guidewire, working as a
+        software architect on our Cloud Assurance and Production Services team.
       </p>
       <p>
         On a daily basis, I work with a language called Gosu. It's a cool little
         JVM language we use here at Guidewire. Outside of my day job, I usually
-        code in Kotlin and JavaScript.
+        code in Kotlin, Java, and JavaScript.
       </p>
       <p>
-        In my spare time, I play football and run (sometimes even in races). I
-        can usually be found checking out new movies and books, or occasionally
-        binge-watching a series on Netflix.
+        In my spare time, I play football and run (although I frequently avoid
+        it). I can usually be found checking out new movies and books, or
+        occasionally binge-watching a series on Netflix.
       </p>
     </div>
   );
--- client/src/components/home/home-view.js
+++ client/src/components/home/home-view.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./home-view.css";
 
-const HomePage = props => {
+const HomePage = (props) => {
   return (
     <div className="gf-home-page">
       <div className="gf-profile-image"></div>
       <p>
         <span className="gf-bold">Hi!</span>
       </p>
-      <p>Thanks for visiting my little corner of the internet.</p>
+      <p>Thanks for visiting. I sometimes write things here.</p>
     </div>
   );
 };
